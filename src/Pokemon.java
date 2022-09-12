ce37e65faded719a994095f798172b3bb1560431
CarsonFarrell26 <carsonfar@yahoo.com>
2022-09-12 09:51:41 -0500

    updated pokemon class

--- src/Pokemon.java
+++ src/Pokemon.java
@@ -1,2 +1,19 @@
 public class Pokemon {
+    Attack firstMove;
+    Attack secondMove;
+    Attack thirdMove;
+    Attack fourthMove;
+    int health;
+    int level;
+    String name;
+
+    public Pokemon(Attack firstMove, Attack secondMove, Attack thirdMove, Attack fourthMove, int baseHealth, String name, int level){
+        this.firstMove = firstMove;
+        this.secondMove = secondMove;
+        this.thirdMove = thirdMove;
+        this.fourthMove = fourthMove;
+        this.health = baseHealth + (level * 2); // Health will scale with level, just like in game. This is much more linear however, gaining 2 HP per level.
+        this.level = level;
+        this.name = name;
+    }
 }
