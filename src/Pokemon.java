05c8a253170d53f9dc9a9a615b44fbe87dd81a67
CarsonFarrell26 <carsonfar@yahoo.com>
2022-09-12 09:56:16 -0500

    getters and setters

--- src/Pokemon.java
+++ src/Pokemon.java
@@ -16,4 +16,53 @@ public class Pokemon {
         this.level = level;
         this.name = name;
     }
+
+    public Attack getFirstMove() {
+        return firstMove;
+    }
+    public void setFirstMove(Attack firstMove) {
+        this.firstMove = firstMove;
+    }
+
+    public Attack getSecondMove() {
+        return secondMove;
+    }
+    public void setSecondMove(Attack secondMove) {
+        this.secondMove = secondMove;
+    }
+
+    public Attack getThirdMove() {
+        return thirdMove;
+    }
+    public void setThirdMove(Attack thirdMove) {
+        this.thirdMove = thirdMove;
+    }
+
+    public Attack getFourthMove() {
+        return fourthMove;
+    }
+    public void setFourthMove(Attack fourthMove) {
+        this.fourthMove = fourthMove;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
 }
