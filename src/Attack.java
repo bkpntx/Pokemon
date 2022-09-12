18bfbe7a014b5bcf279487d1e50a566501fb416c
bkpntx <bkpntx@gmail.com>
2022-09-12 09:48:48 -0500

    attack class

--- src/Attack.java
+++ src/Attack.java
@@ -1,2 +1,24 @@
 public class Attack {
+    private int damage;
+    private boolean inflictsBurn;
+    private boolean inflictsPoison;
+
+    public boolean isInflictsBurn() {
+        return inflictsBurn;
+    }
+    public int getDamage() {
+        return damage;
+    }
+    public boolean isInflictsPoison() {
+        return inflictsPoison;
+    }
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+    public void setInflictsBurn(boolean inflictsBurn) {
+        this.inflictsBurn = inflictsBurn;
+    }
+    public void setInflictsPoison(boolean inflictsPoison) {
+        this.inflictsPoison = inflictsPoison;
+    }
 }
