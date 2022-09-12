3b29700e8e3669c3f96cfbf2cb1c31e085c2cde6
Harris <hlee@sjs.org>
2022-09-12 09:54:32 -0500

    constructors for item and pokeball

--- src/Pokeball.java
+++ src/Pokeball.java
@@ -1,2 +1,6 @@
-public class Pokeball {
+public class Pokeball extends Item{
+    // once Pokeball is used (if Pokeball), boolean isGameOver should become true
+    Pokeball(int healing){
+        super(healing);
+    }
 }
