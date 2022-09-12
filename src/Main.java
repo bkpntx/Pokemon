6afc622e2fc21693a3d436a28c61ff0ebc71e294
bkpntx <bkpntx@gmail.com>
2022-09-12 10:14:24 -0500

    adding some attacks and print stuff

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,16 @@ public class Main {
     static Attack blueFlare = new Attack(130,true,false);
     static Attack branchPoke = new Attack(40,false,false);
     static Attack gunkShot = new Attack(85,false,true);
+    public static void fill(String current){
+        while (current.length()<29){
+            current=current+" ";
+        }
+        current=current+"*";
+    }
+    public static void basePrint(){
+        System.out.println("*****************************");
+        System.out.println("*"+);
+    }
     public static void main(String[] args) {
 
     }
