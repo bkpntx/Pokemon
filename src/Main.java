d621327f09786a0d23c735dd080e30cb1a74420f
bkpntx <bkpntx@gmail.com>
2022-09-12 10:03:39 -0500

    adding some attacks

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     static Attack tackle = new Attack(35,false,false);
     static Attack accelerock = new Attack(40,false,false);
+    static Attack blueFlare = new Attack(130,true,false);
+    static Attack branchPoke = new Attack(40,false,false);
+    static Attack gunkShot = new Attack(85,false,true);
     public static void main(String[] args) {
 
     }
