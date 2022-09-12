9ddd3a390f25177f1b6a6be933c587f025a6a21b
CarsonFarrell26 <carsonfar@yahoo.com>
2022-09-12 10:08:21 -0500

    more attacks

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,11 @@ public class Main {
     static Attack tackle = new Attack(35,false,false);
     static Attack accelerock = new Attack(40,false,false);
     static Attack blueFlare = new Attack(130,true,false);
+    static Attack willOWisp = new Attack (0, true, false); // Will-O-Wisp is a non damaging move.
     static Attack branchPoke = new Attack(40,false,false);
     static Attack gunkShot = new Attack(85,false,true);
+    static Attack sludgeBomb = new Attack(90, false, true);
+
     public static void main(String[] args) {
 
     }
